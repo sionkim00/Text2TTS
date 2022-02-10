bb453b5c348d8abc74c7211f037a59a7db3d3532
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-02-10 11:33:16 +0900

    add core logics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Timer from "./components/Timer";
+import { useStopwatch } from "react-timer-hook";
 
 export default function App() {
   const [timers, setTimers] = useState([
@@ -7,30 +8,58 @@ export default function App() {
     { text: "hello", time: 1 }
   ]);
 
+  const { seconds, isRunning, start, reset } = useStopwatch({
+    autoStart: false
+  });
+  const maxTime = Math.max(...timers.map((t) => t.time), 0); //Find largest time
+
+  useEffect(() => {
+    let findTimerOnTime = timers.find((t) => t.time === seconds);
+    console.log(findTimerOnTime);
+
+    if (maxTime <= seconds) reset(new Date(), false); //End if out of bound
+  }, [seconds]);
+
   function handleAdd() {
-    setTimers((timers) => [...timers, { text: "", time: null }]);
+    setTimers((timers) => [...timers, { text: "", time: 0 }]);
   }
+  function updateTimer(index, text, time) {
+    let newTimers = [...timers];
+    newTimers[index].text = text;
+    newTimers[index].time = time;
+    setTimers(newTimers);
+  }
+
   return (
     <div className="App">
       <h1>Text2TTS</h1>
 
+      {/* timers */}
       <div className="timers">
         {timers.map((timer, i) => (
-          <Timer timer={timer} i={i} />
+          <Timer timer={timer} i={i} updateTimer={updateTimer} key={i} />
         ))}
         <button onClick={handleAdd}>
-          <i class="fa-solid fa-plus"></i>
+          <i className="fa-solid fa-plus"></i>
           <span>Add</span>
         </button>
       </div>
 
       {/* seconds */}
-      <p>0</p>
+      <p>{seconds}</p>
 
-      {/* Start/Stop button */}
-      <button>
-        <i class="fa-solid fa-play"></i>
-      </button>
+      {/* Start/Stop button and isRunning */}
+      <div>
+        {isRunning ? (
+          <button onClick={() => reset(new Date(), false)}>
+            <i className="fa-solid fa-stop"></i>
+          </button>
+        ) : (
+          <button onClick={start}>
+            <i className="fa-solid fa-play"></i>
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-export default function Timer({ timer, i }) {
-  const [text, setText] = useState(null);
-  const [time, setTime] = useState(null);
+export default function Timer({ timer, i, updateTimer }) {
+  const [text, setText] = useState("null");
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
     setText(timer.text);
@@ -14,13 +14,15 @@ export default function Timer({ timer, i }) {
         type="number"
         placeholder="Time"
         value={time}
-        onChange={(e) => setTime(e.target.value)}
+        onChange={(e) => setTime(parseInt(e.target.value))}
+        onBlur={() => updateTimer(i, text, time)}
       />
       <input
         type="text"
         placeholder="Set a text"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onBlur={() => updateTimer(i, text, time)}
       />
     </form>
   );
